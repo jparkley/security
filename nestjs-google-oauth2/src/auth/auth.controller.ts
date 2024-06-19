87026b0a349f64b216a198ec5f15eba0c56a20b3
jparkley <parkleydondon@yahoo.com>
2024-06-19 10:24:22 -0700

    add Guard for Google

--- nestjs-google-oauth2/src/auth/auth.controller.ts
+++ nestjs-google-oauth2/src/auth/auth.controller.ts
@@ -1,12 +1,15 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { GoogleAuthGuard } from './passport/google-auth-guard';
 
 @Controller('auth')
 export class AuthController {
   @Get('google/login')
+  @UseGuards(GoogleAuthGuard)
   login() {
     return { msg: 'goole oauth est' };
   }
 
   @Get('google/redirect')
+  @UseGuards(GoogleAuthGuard)
   redirect() {}
 }
