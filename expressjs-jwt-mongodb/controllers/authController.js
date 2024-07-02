9b428a47f254a84cd2efa88beba8d8f30becf7f9
jparkley <parkleydondon@yahoo.com>
2024-07-02 14:36:57 -0700

    add user model, add POST /signup

--- expressjs-jwt-mongodb/controllers/authController.js
+++ expressjs-jwt-mongodb/controllers/authController.js
@@ -1,16 +1,45 @@
+const User = require("../models/User");
+
+const handleErrors = (error) => {
+  // console.log("error: ", error);
+
+  let errors = { email: "", password: "" };
+  if (error.code === 11000) {
+    errors.email = "Email already registered. Please try another";
+    return errors;
+  }
+
+  if (error.message.includes("user validation failed")) {
+    Object.values(error.errors).forEach(({ properties }) => {
+      errors[properties.path] = properties.message;
+    });
+  }
+  return errors;
+};
+
 function getSignup(req, res) {
   res.render("signup");
 }
 
-function postSignup(req, res) {
-  res.send("post signup");
-}
+const postSignup = async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await User.create({ email, password });
+    res.status(201).json(user);
+  } catch (error) {
+    const errors = handleErrors(error);
+    res.status(400).json({ errors });
+  }
+};
 
 function getLogin(req, res) {
   res.render("login");
 }
 
 function postLogin(req, res) {
+  const { email, password } = req.body;
+  console.log("--=======---", email, password);
   res.send("post login");
 }
 
--- expressjs-jwt-mongodb/index.js
+++ expressjs-jwt-mongodb/index.js
@@ -6,6 +6,7 @@ const mongoose = require("mongoose");
 const authRoutes = require("./routes/authRoutes");
 
 const app = express();
+app.use(express.json());
 
 app.use(express.static("public"));
 app.set("view engine", "ejs");
